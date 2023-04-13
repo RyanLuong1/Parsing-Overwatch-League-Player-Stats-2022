b913a6e65e3361faccb8b0caa46e34b838a73824
Ryan Luong <ryanluong83@gmail.com>
2023-04-13 13:45:00 -0700

    Seperated the heroes usage based on qualifiers and tournament

--- get_heroes_usage.py
+++ get_heroes_usage.py
@@ -80,6 +80,15 @@ summer_showdown_heroes = summer_showdown_df.drop_duplicates(subset=['esports_mat
 summer_showdown_global_heroes_usage = summer_showdown_heroes.groupby(["hero_name"]) \
                                       .size() \
                                       .to_frame("total_times_played")
+summer_showdown_qualifiers_heroes_usage = summer_showdown_heroes[summer_showdown_heroes["tournament_title"] == "Summer Showdown: Qualifiers"] \
+                                        .groupby(["hero_name"]) \
+                                        .size() \
+                                        .to_frame("total_times_played")
+summer_showdown_tournament_heroes_usage = summer_showdown_heroes[summer_showdown_heroes["tournament_title"] == "Summer Showdown: Tournament"] \
+                                        .groupby(["hero_name"]) \
+                                        .size() \
+                                        .to_frame("total_times_played")
+
 summer_showdown_qualifiers_support_players_heroes_usage = summer_showdown_heroes[summer_showdown_heroes.hero_name.isin(support_heroes)] \
                                             .loc[summer_showdown_heroes.tournament_title != "Summer Showdown: Qualifiers"] \
                                             .groupby(["player_name", "team_name", "hero_name"]) \
@@ -132,7 +141,17 @@ summer_showdown_tournament_tank_players_heroes_usage["total_times_played"] = sum
 midseason_madness_heroes = midseason_madness_df.drop_duplicates(subset=['esports_match_id', "map_name", "map_type", "player_name", "team_name", "hero_name", "tournament_title"]) \
                        .loc[midseason_madness_df.hero_name != "All Heroes"] \
                        .loc[:, ["map_name", "player_name", "team_name", "hero_name", "tournament_title"]]
-midseason_madness_global_heroes_usage = midseason_madness_heroes.groupby(["hero_name"]).size().to_frame("total_times_played")
+midseason_madness_qualifiers_heroes_usage = midseason_madness_heroes[midseason_madness_heroes["tournament_title"] == "Midseason Madness: Qualifiers"] \
+                                        .groupby(["hero_name"]) \
+                                        .size() \
+                                        .to_frame("total_times_played")
+midseason_madness_tournament_heroes_usage = midseason_madness_heroes[midseason_madness_heroes["tournament_title"] == "Midseason Madness: Tournament"] \
+                                        .groupby(["hero_name"]) \
+                                        .size() \
+                                        .to_frame("total_times_played")
+midseason_madness_global_heroes_usage = midseason_madness_heroes.groupby(["hero_name"]) \
+                                        .size() \
+                                        .to_frame("total_times_played")
 midseason_madness_qualifiers_support_players_heroes_usage = midseason_madness_heroes[midseason_madness_heroes.hero_name.isin(support_heroes)] \
                                             .loc[midseason_madness_heroes.tournament_title != "Midseason Madness: Qualifiers"] \
                                             .groupby(["player_name", "team_name", "hero_name"]) \
@@ -182,9 +201,9 @@ midseason_madness_tournament_tank_players_heroes_usage["total_times_played"] = m
 countdown_cup_heroes = countdown_cup_df.drop_duplicates(subset=['esports_match_id', "map_name", "map_type", "player_name", "team_name", "hero_name"]) \
                        .loc[countdown_cup_df.hero_name != "All Heroes"] \
                        .loc[:, ["map_name", "player_name", "team_name", "hero_name"]]
-countdown_cup_global_heroes_usage = countdown_cup_heroes.groupby(["hero_name"]) \
-                                    .size() \
-                                    .to_frame("total_times_played")
+countdown_cup_qualifiers_heroes_usage = countdown_cup_heroes.groupby(["hero_name"]) \
+                                        .size() \
+                                        .to_frame("total_times_played")
 countdown_cup_qualifiers_support_players_heroes_usage = countdown_cup_heroes[countdown_cup_heroes.hero_name.isin(support_heroes)] \
                                             .groupby(["player_name", "team_name", "hero_name"]) \
                                             .size() \
@@ -207,7 +226,7 @@ countdown_cup_qualifiers_tank_players_heroes_usage["total_times_played"] = count
 grand_finals_heroes = grand_finals_df.drop_duplicates(subset=['esports_match_id', "map_name", "map_type", "player_name", "team_name", "hero_name"]) \
                        .loc[grand_finals_df.hero_name != "All Heroes"] \
                        .loc[:, ["map_name", "player_name", "team_name", "hero_name"]]
-grand_finals_global_heroes_usage = grand_finals_heroes.groupby(["hero_name"]) \
+grand_finals_heroes_usage = grand_finals_heroes.groupby(["hero_name"]) \
                                    .size() \
                                    .to_frame("total_times_played")
 grand_finals_support_players_heroes_usage = grand_finals_heroes[grand_finals_heroes.hero_name.isin(support_heroes)] \
@@ -231,7 +250,9 @@ grand_finals_tank_players_heroes_usage["total_times_played"] = grand_finals_tank
 
 
 kickoff_clash_heroes.to_csv("kickoff-clash/main/kickoff-clash-heroes.csv")
-kickoff_clash_global_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-global-heroes-usage.csv")
+# kickoff_clash_global_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-global-heroes-usage.csv")
+kickoff_clash_qualifiers_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-qualifiers-heroes-usage.csv")
+kickoff_clash_tournament_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-tournament-heroes-usage.csv")
 kickoff_clash_qualifiers_support_players_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-qualifiers-support-players-heroes-usage.csv")
 kickoff_clash_qualifiers_dps_players_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-qualifiers-dps-players-heroes-usage.csv")
 kickoff_clash_qualifiers_tank_players_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-qualifiers-tank-players-heroes-usage.csv")
@@ -243,7 +264,9 @@ kickoff_clash_tournament_tank_players_heroes_usage.to_csv("kickoff-clash/heroes-
 # kickoff_clash_tank_players_heroes_usage.to_csv("kickoff-clash/heroes-usage/kickoff-clash-tank-players-heroes-usage.csv")
 
 midseason_madness_heroes.to_csv("midseason-madness/main/midseason-madness-heroes.csv")
-midseason_madness_global_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-global-heroes-usage.csv")
+# midseason_madness_global_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-global-heroes-usage.csv")
+midseason_madness_qualifiers_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-qualifiers-heroes-usage.csv")
+midseason_madness_tournament_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-tournament-heroes-usage.csv")
 midseason_madness_qualifiers_support_players_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-qualifiers-support-players-heroes-usage.csv")
 midseason_madness_qualifiers_dps_players_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-qualifiers-dps-players-heroes-usage.csv")
 midseason_madness_qualifiers_tank_players_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-qualifiers-tank-players-heroes-usage.csv")
@@ -255,7 +278,9 @@ midseason_madness_tournament_tank_players_heroes_usage.to_csv("midseason-madness
 # midseason_madness_tank_players_heroes_usage.to_csv("midseason-madness/heroes-usage/midseason-madness-tank-players-heroes-usage.csv")
 
 summer_showdown_heroes.to_csv("summer-showdown/main/summer-showdown-heroes.csv")
-summer_showdown_global_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-global-heroes-usage.csv")
+# summer_showdown_global_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-global-heroes-usage.csv")
+summer_showdown_qualifiers_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-qualifiers-heroes-usage.csv")
+summer_showdown_tournament_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-tournament-heroes-usage.csv")
 summer_showdown_qualifiers_support_players_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-qualifiers-support-players-heroes-usage.csv")
 summer_showdown_qualifiers_dps_players_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-qualifiers-dps-players-heroes-usage.csv")
 summer_showdown_qualifiers_tank_players_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-qualifiers-tank-players-heroes-usage.csv")
@@ -267,7 +292,7 @@ summer_showdown_tournament_tank_players_heroes_usage.to_csv("summer-showdown/her
 # summer_showdown_tank_players_heroes_usage.to_csv("summer-showdown/heroes-usage/summer-showdown-tank-players-heroes-usage.csv")
 
 countdown_cup_heroes.to_csv("countdown-cup/main/countdown-cup-heroes.csv")
-countdown_cup_global_heroes_usage.to_csv("countdown-cup/heroes-usage/countdown-cup-global-heroes-usage.csv")
+countdown_cup_qualifiers_heroes_usage.to_csv("countdown-cup/heroes-usage/countdown-cup-qualifiers-heroes-usage.csv")
 countdown_cup_qualifiers_support_players_heroes_usage.to_csv("countdown-cup/heroes-usage/countdown-cup-qualifiers-support-players-heroes-usage.csv")
 countdown_cup_qualifiers_dps_players_heroes_usage.to_csv("countdown-cup/heroes-usage/countdown-cup-qualifiers-dps-players-heroes-usage.csv")
 countdown_cup_qualifiers_tank_players_heroes_usage.to_csv("countdown-cup/heroes-usage/countdown-cup-qualifiers-tank-players-heroes-usage.csv")
@@ -276,7 +301,7 @@ countdown_cup_qualifiers_tank_players_heroes_usage.to_csv("countdown-cup/heroes-
 # countdown_cup_tank_players_heroes_usage.to_csv("countdown-cup/heroes-usage/countdown-cup-tank-players-heroes-usage.csv")
 
 grand_finals_heroes.to_csv("grand-finals/main/grand-finals-heroes.csv")
-grand_finals_global_heroes_usage.to_csv("grand-finals/heroes-usage/grand-finals-global-heroes-usage.csv")
+grand_finals_heroes_usage.to_csv("grand-finals/heroes-usage/grand-finals-heroes-usage.csv")
 grand_finals_support_players_heroes_usage.to_csv("grand-finals/heroes-usage/grand-finals-support-players-heroes-usage.csv")
 grand_finals_dps_players_heroes_usage.to_csv("grand-finals/heroes-usage/grand-finals-dps-players-heroes-usage.csv")
 grand_finals_tank_players_heroes_usage.to_csv("grand-finals/heroes-usage/grand-finals-tank-players-heroes-usage.csv")
